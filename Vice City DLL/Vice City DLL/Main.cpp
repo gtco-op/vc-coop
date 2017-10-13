7b93751db4124ef998c8a25cc74dfdee8a7bfa82
Sebastian Klemens <sebaklemens1@gmail.com>
2017-10-13 17:36:38 +0200

    Update Main.cpp
    Added some useful functions

--- Vice City DLL/Vice City DLL/Main.cpp
+++ Vice City DLL/Vice City DLL/Main.cpp
@@ -30,6 +30,44 @@ public:
 		CWorld::Add(ped);
 		return id;
 	}
+		int SpawnPed(unsigned int modelindex, CVector coords, ePedType pedtype)
+	{
+		int id;
+		CPed *ped = CPopulation::AddPed(pedtype, modelindex, coords, 0);
+		CWorld::Add(ped);
+		
+	}
+	int CreateCoupleOfPeds(ePedType PedType, unsigned int Model, ePedType PedType2, unsigned int Model2, CVector Position)
+	{
+		CPopulation::PlaceCouple(PedType, Model, PedType2, Model2, Position);
+	}
+	int GivePedrWeapon(CPed Ped, eWeaponType WepType, unsigned int Ammo)
+	{
+	 // this needs some load model work
+		Ped.GiveWeapon(WepType, Ammo, false);
+		Ped.SetCurrentWeapon(WepType);
+	}
+	int KillPed(CPed Ped)
+	{
+		Ped.m_fHealth = 0.0;
+	}
+	int TeleportPed(CPed Ped, float PosX, float PosY, float PosZ)
+	{
+		Ped.Teleport(CVector(PosX, PosY, PosZ));
+	}
+	int PedSetAimAtEntity(CPed Ped, CEntity Entity)
+	{
+		Ped.m_pPointGunAt = &Entity;
+		Ped.AimGun();
+	}
+	int SetPedHealth(CPed Ped, float Amount)
+	{
+		Ped.m_fHealth = Amount;
+	}
+	int SetPedArmour(CPed Ped, float Amount)
+	{
+		Ped.m_fArmour = Amount;
+	}
     MyPlugin() {
 
 
