81c8b3cb8c2b65285fc32d738de89962fb273424
LemonHaze420 <lh@aestheticstudios.co.uk>
2018-03-24 00:43:50 +0000

    Added GetRandomVehicleColor() to CModelIDs and fixed server ped model limit

--- vice-city-coop/server/CModelIDs.h
+++ vice-city-coop/server/CModelIDs.h
@@ -3,6 +3,7 @@
 #define MAX_OBJ_MODEL_ID 288
 #define MAX_PED_MODEL_ID 174
 #define MAX_VEH_MODEL_ID 104
+#define MAX_VEH_COLOR_ID 95
 
 enum eModelType {
 	MODEL_OBJ,
@@ -21,9 +22,11 @@ public:
 	static bool IsValidVehicleModel(int modelID);
 	
 	static int GetRandomModel(eModelType type = MODEL_PED);
+	static int GetRandomVehicleColor();
 
 	static const int objModelIDs[MAX_OBJ_MODEL_ID];
 	static const int pedModelIDs[MAX_PED_MODEL_ID];
 	static const int vehModelIDs[MAX_VEH_MODEL_ID];
+	static const int vehColorIDs[MAX_VEH_COLOR_ID];
 };
 
