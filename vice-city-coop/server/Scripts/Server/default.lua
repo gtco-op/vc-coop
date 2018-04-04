01044975e4802fe240138d36067b1e370d951ec1
LemonHaze420 <lh@aestheticstudios.co.uk>
2018-04-04 02:54:33 +0100

    Updated example gamemode script

--- vice-city-coop/server/Scripts/Server/default.lua
+++ vice-city-coop/server/Scripts/Server/default.lua
@@ -6,6 +6,7 @@ function onServerStart()
 	local randColorB = GetRandomVehicleColor()
 	
 	AddVehicle(modelID, 538.117798, 626.908203, 10.901563, randColorA, randColorB)
+	AddVehicle(modelID, 540.117798, 630.908203, 10.901563, randColorA, randColorB)
 end
 function onPlayerConnect(id)
 	print("Player "..GetPlayerName(id).. " joined!")
@@ -20,5 +21,14 @@ function onPlayerMessage(playerid, msg)
 	
 	if (matchText) then
 		SetPlayerModel(playerid, matchText)		
+	end
+	
+	matchText,ammo = string.match(msg, "/weapon (%d+) (%d+)")
+	if (matchText and ammo) then
+		GivePlayerWeapon(playerid, matchText, ammo)		
+		SendPlayerMessage(playerid, "Given weapon "..matchText.." with "..ammo.." ammo.")
+	else
+		SendPlayerMessage(playerid, "Incorrect arguments!")
+		SendPlayerMessage(playerid, "/weapon <weapon id> (ammo)")
 	end
 end
