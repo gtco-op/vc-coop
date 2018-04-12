5f8c1c4a46a293b6fe85368b06192051d6196123
LemonHaze420 <lh@aestheticstudios.co.uk>
2018-04-12 18:35:05 +0100

    Added original function description for CommandLineToArgvA()

--- vice-city-coop/vccoop_main/Main.cpp
+++ vice-city-coop/vccoop_main/Main.cpp
@@ -5,6 +5,34 @@ CCore *core = nullptr;
 sStartParams gStartParams;
 
 
+/*************************************************************************
+* CommandLineToArgvA            [SHELL32.@]
+*
+* MODIFIED FROM https://www.winehq.org/ project
+* We must interpret the quotes in the command line to rebuild the argv
+* array correctly:
+* - arguments are separated by spaces or tabs
+* - quotes serve as optional argument delimiters
+*   '"a b"'   -> 'a b'
+* - escaped quotes must be converted back to '"'
+*   '\"'      -> '"'
+* - consecutive backslashes preceding a quote see their number halved with
+*   the remainder escaping the quote:
+*   2n   backslashes + quote -> n backslashes + quote as an argument delimiter
+*   2n+1 backslashes + quote -> n backslashes + literal quote
+* - backslashes that are not followed by a quote are copied literally:
+*   'a\b'     -> 'a\b'
+*   'a\\b'    -> 'a\\b'
+* - in quoted strings, consecutive quotes see their number divided by three
+*   with the remainder modulo 3 deciding whether to close the string or not.
+*   Note that the opening quote must be counted in the consecutive quotes,
+*   that's the (1+) below:
+*   (1+) 3n   quotes -> n quotes
+*   (1+) 3n+1 quotes -> n quotes plus closes the quoted string
+*   (1+) 3n+2 quotes -> n+1 quotes plus closes the quoted string
+* - in unquoted strings, the first quote opens the quoted string and the
+*   remaining consecutive quotes follow the above rule.
+*/
 LPSTR* WINAPI CommandLineToArgvA(LPSTR lpCmdline, int* numargs)
 {
 	DWORD argc;
